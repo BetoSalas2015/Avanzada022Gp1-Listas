cc3164af2d0a7c21fe40fd1ca60404115db3fc6f
Roberto Salazar Márquez <roberto.salazar@upaep.mx>
2022-09-27 12:56:38 -0500

    Sesión 18 - 26/09/2022

--- Listas.c
+++ Listas.c
@@ -1,4 +1,4 @@
-//  Sesión 17 - 23/09/2022
+//  Sesión 18 - 27/09/2022
 #include <stdio.h>				//  Para la I/O de programa
 #include <stdlib.h>				//  Para las rutinas de utileria
 #include <malloc.h>				//  Para la gestión de memoria dinámica
@@ -20,13 +20,56 @@ void insert(char dato)
 {
 	nodo *nuevo = NULL;
 	nuevo = (nodo *) malloc( sizeof(nodo) );
+	if( nuevo == NULL )				//  No hay memoria disponible
+	{
+		printf("Nohay memoria disponible! \n");
+		pausa;
+		exit(1);		//	Código de error personalizado 1. No hay memoria
+	}
+	nuevo -> info = dato;		//  Preservamos la información en la lista
+	if( raiz == NULL )			//  Lista vacía, no hay nodos
+	{
+		raiz = nuevo;			//  Insertamos el nodo en la lista
+		raiz ->sig = NULL;		//  Se vuelve el último nodo de la lista
+	}
+	else				//  La lista no esta vacía, hay al menos 1 nodo
+	{
+		nodo *recorre = raiz;
+		while(recorre ->sig != NULL)		// Estoy posicionado en el ultimo nodo?
+		{
+			recorre = recorre-> sig; 
+		}
+		recorre->sig = nuevo;		// Insertamos el nodo en la lista ligada
+		nuevo ->sig = NULL;
+	}
+}
+
+void imprimeLista()
+{
+	nodo *recorre = raiz;
+	while(recorre != NULL)		// Estoy posicionado en el ultimo nodo?
+	{
+		printf("%c, ", recorre->info );
+		recorre = recorre-> sig; 
+	}
+	putchar('\n');
 }
 
 int main()
-{
-	
-
+{	
 	system("color 7");
+	imprimeLista();
+	insert('R');
+	insert('o');
+	insert('b');
+	insert('e');
+	insert('r');
+	insert('t');
+	insert('o');
+
+	imprimeLista();
+
+	
 	pausa;
 	return 0;
 }
